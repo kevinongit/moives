9b04b490c350e4900bffe59044fa63f0a841a008
kevin <covaloper@gmail.com>
2020-02-17 23:15:24 +0900

    test added

---
+++ a.js
@@ -0,0 +1,2 @@
+const arr = [1,2,3,4,5].map(x => x*2).filter(x => x > 5);
+console.log(`arr = ${arr}`);
---
+++ test/persons.js
@@ -0,0 +1,12 @@
+
+const persons = [
+  { name: 'Peter', age: 16 },
+  { name: 'Mark', age: 18 },
+  { name: 'John', age: 27 },
+  { name: 'Jane', age: 14 },
+  { name: 'Tony', age: 24},
+];
+
+// Q: code for getting adults if adulst => age >= 18
+
+console.log(adults);
